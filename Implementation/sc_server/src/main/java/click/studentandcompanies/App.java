178c52d163d179a155daa25ea0b39a4ea8af765f
Slaitroc <slaitroc@gmail.com>
2024-11-20 17:06:10 +0100

    Implementation Project Setup

---
+++ Implementation/sc_server/src/main/java/click/studentandcompanies/App.java
@@ -0,0 +1,13 @@
+package click.studentandcompanies;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Implementation/sc_server/src/main/java/click/studentandcompanies/Controller.java
@@ -0,0 +1,13 @@
+package click.studentandcompanies;
+
+/**
+ * Hello world!
+ *
+ */
+public class Controller 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
