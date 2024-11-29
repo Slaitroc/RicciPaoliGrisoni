b318c10b031ba6d6554cddb7313d2064fddf8250
Slaitroc <slaitroc@gmail.com>
2024-11-29 19:01:59 +0100

    Update Backend Containers Refactor docker-compose.yml & Dockerfile Refactor mvn pom.xml Refactor App.java & Controller.java Add application.properties for Spring Boot build Refactor sc_server .gitignore

--- Implementation/sc_server/src/main/java/click/studentandcompanies/Controller.java
+++ Implementation/sc_server/src/main/java/click/studentandcompanies/Controller.java
@@ -1,13 +1,14 @@
 package click.studentandcompanies;
 
-/**
- * Hello world!
- *
- */
-public class Controller 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, Spring Boot!";
     }
 }
