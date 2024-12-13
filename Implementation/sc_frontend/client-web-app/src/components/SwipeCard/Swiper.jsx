82814f72f429f959432430ffd4f0946f53da3c6d
Samuele <131958127+dedepivot@users.noreply.github.com>
2024-12-14 00:16:44 +0100

    Add basic motion effect

--- Implementation/sc_frontend/client-web-app/src/components/SwipeCard/Swiper.jsx
+++ Implementation/sc_frontend/client-web-app/src/components/SwipeCard/Swiper.jsx
@@ -1,27 +1,35 @@
 import React from 'react';
 import './Swiper.css';
 import CardTemplate from './CardTemplate';
-import {motion} from 'framer-motion';
+import { motion, useMotionValue, useTransform } from 'framer-motion';
 
 function SwipeCard({ items }) {
     return (
         <div className="mainContainer">
             <div className="cardContainer">
-                {items.map((card, index) => (
-                    <motion.div key={index} className="swipeableCard"
+                {items.map((card, index) => {
+                    const x = useMotionValue(0);
+                    const opacity = useTransform(x, [-200, 0, 200], [0, 1, 0]);
+                    const rotate = useTransform(x, [-200, -40, 0, 40, 200], [-18, 0, 0, 0, 18]);
+                    return (<motion.div key={index} className="swipeableCard"
                         style={{
                             zIndex: items.length - index,
+                            x,
+                            opacity,
+                            rotate
                         }}
                         drag="x"
                         dragConstraints={{ left: 0, right: 0 }}
-                        >
+                    >
                         <CardTemplate
                             companyName={card.companyName}
                             position={card.position}
                             description={card.description}
                         />
-                    </motion.div>
-                ))}
+                    </motion.div>)
+
+                }
+                )}
             </div>
         </div>
     );
