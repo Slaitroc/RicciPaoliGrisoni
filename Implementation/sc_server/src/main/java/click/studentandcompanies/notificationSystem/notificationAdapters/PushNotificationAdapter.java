9f3027704276a718c03011e97c5ceb5633833c92
dedepivot <samuele.grisoni@gmail.com>
2025-01-25 12:54:03 +0100

    add firebase notification implementation (to be tested)

--- Implementation/sc_server/src/main/java/click/studentandcompanies/notificationSystem/notificationAdapters/PushNotificationAdapter.java
+++ Implementation/sc_server/src/main/java/click/studentandcompanies/notificationSystem/notificationAdapters/PushNotificationAdapter.java
@@ -1,6 +1,8 @@
 package click.studentandcompanies.notificationSystem.notificationAdapters;
 
 import click.studentandcompanies.notificationSystem.notificationUtils.NotificationPayload;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.Message;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,8 +10,19 @@ import java.util.List;
 @Service
 public class PushNotificationAdapter {
     public void sendPushNotification(List<String> deviceTokens, NotificationPayload payload) {
-        // todo implement the actual firebase push notification sending
-        System.out.println("Sending push notification to " + deviceTokens.size() + " devices:\n" + deviceTokens);
-        System.out.println("Payload: " + payload.title() + "\n" + payload.body() +  "\n\n");
+        for (String deviceToken : deviceTokens) {
+            Message message = createMessage(deviceToken, payload);
+            FirebaseMessaging.getInstance().sendAsync(message);
+            System.out.println("Sent push notification to " + deviceToken);
+            System.out.println("Payload: " + payload.title() + "\n" + payload.body() +  "\n\n");
+        }
+    }
+
+    private Message createMessage(String deviceToken, NotificationPayload payload) {
+        return Message.builder()
+            .setToken(deviceToken)
+            .putData("title", payload.title())
+            .putData("body", payload.body())
+            .build();
     }
 }
