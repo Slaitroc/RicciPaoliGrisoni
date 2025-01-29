aa368f673a33afab98fe322ea70cdff4501df48e
matte <matteog.paoli@gmail.com>
2025-01-30 00:17:29 +0100

    added note sus warning

--- Implementation/sc_server/src/main/java/click/studentandcompanies/entityRepository/InternshipPosOfferRepository.java
+++ Implementation/sc_server/src/main/java/click/studentandcompanies/entityRepository/InternshipPosOfferRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InternshipPosOfferRepository extends JpaRepository<InternshipPosOffer, Integer> {
 
+    //NOTE weird warnings? dangerous?
     InternshipPosOffer getById(Integer id);
 }
