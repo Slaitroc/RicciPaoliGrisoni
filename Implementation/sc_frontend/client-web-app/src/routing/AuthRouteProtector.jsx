a270da7aa88f056e259539ce94657d27ef15716b
matte <matteog.paoli@gmail.com>
2025-01-30 12:55:58 +0100

    Refactor route protectors to include useLocation; enhance loading state handling and debug logging

--- Implementation/sc_frontend/client-web-app/src/routing/AuthRouteProtector.jsx
+++ Implementation/sc_frontend/client-web-app/src/routing/AuthRouteProtector.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useGlobalContext } from "../global/GlobalContext";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { CircularProgress } from "@mui/material";
 
 const AuthRouteProtector = ({
@@ -9,18 +9,19 @@ const AuthRouteProtector = ({
   invertBehavior = false,
 }) => {
   const { isAuthenticated, loading, setLoading } = useGlobalContext();
+  const location = useLocation();
 
-  // console.log("loading", loading);
+  console.log("AUTH- isAuthenticated:", isAuthenticated);
+  console.log("AUTH- Loading status:", loading);
 
   if (loading) return <CircularProgress size="3rem" />;
+  else {
+    if (invertBehavior) {
+      return isAuthenticated ? <Navigate to={redirectTo} replace /> : children;
+    }
 
-  // setLoading(true);
-
-  if (invertBehavior) {
-    return isAuthenticated ? <Navigate to={redirectTo} replace /> : children;
+    return isAuthenticated ? children : <Navigate to={redirectTo} replace />;
   }
-
-  return isAuthenticated ? children : <Navigate to={redirectTo} replace />;
 };
 
 export default AuthRouteProtector;
