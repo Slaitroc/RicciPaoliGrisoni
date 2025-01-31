3b1ce7f921ef67b55c88edfd2ddc0ffbb581fe0c
matte <matteog.paoli@gmail.com>
2025-01-31 23:33:17 +0100

    add logging for communications data in CommunicationsList component

--- Implementation/sc_frontend/client-web-app/src/components/Communications/CommunicationsList.jsx
+++ Implementation/sc_frontend/client-web-app/src/components/Communications/CommunicationsList.jsx
@@ -9,19 +9,22 @@ export const CommunicationsList = () => {
   const { communicationsData } = useCommunicationsContext();
 
   return (
-    <List
-      sx={{
-        width: "100%",
-        bgcolor: "background.paper",
-        position: "relative",
-        overflow: "auto",
-        maxHeight: 550,
-      }}
-    >
-      {communicationsData?.map((data) => (
-        <SCCommunicationItem key={data.id} communication={data} />
-      ))}
-    </List>
+    logger.log("data: " + communicationsData),
+    (
+      <List
+        sx={{
+          width: "100%",
+          bgcolor: "background.paper",
+          position: "relative",
+          overflow: "auto",
+          maxHeight: 550,
+        }}
+      >
+        {communicationsData?.map((data) => (
+          <SCCommunicationItem key={data.id} communication={data} />
+        ))}
+      </List>
+    )
   );
 };
 
