988814890c2758306ed7613f2fd37532b6f6179a
Slaitroc <slaitroc@gmail.com>
2025-02-04 12:35:37 +0100

    update apiConfig.js to use localhost for staging environment

--- Implementation/sc_frontend/client-web-app/src/api-calls/apiConfig.js
+++ Implementation/sc_frontend/client-web-app/src/api-calls/apiConfig.js
@@ -2,15 +2,13 @@ const getBaseDomain = () => {
   if (import.meta.env.VITE_USE_STUBS === "true") {
     return "http://localhost:5173";
   } else if (import.meta.env.VITE_USE_STUBS === "false") {
-    return "http://10.147.19.176"; // Ambiente di staging
+    return "http://localhost"; // Ambiente di staging
   }
 };
 
 const getFRontendDomain = () => {
   return "http://localhost:5173";
-}
-
-
+};
 
 export const BASE_DOMAIN = getBaseDomain();
 export const FRONTEND_DOMAIN = getFRontendDomain();
